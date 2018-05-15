df0233cc37ae5498998870fceba07be47cb41a63
marb61a <marb61a@gmail.com>
2018-05-15 11:50:15 +0100

    addedauth models and updated auth service with login and register methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,10 +7,43 @@ import { AuthData } from './auth-data.model';
 
 @Injectable()
 export class AuthService {
+  authChange = new Subject<boolean>();
+  private user: User;
 
   constructor(private router: Router) {}
 
-  login(authData: AuthData) {
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
 
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
+
+  logout() {
+    this.user = null;
+    this.authChange.next(false);
+    this.router.navigate(['/login']);
+  }
+
+  getUser() {
+    return {...this.user};
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 }

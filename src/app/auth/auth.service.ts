a887e35860e20444409665bd450a3881f3b34002
marb61a <marb61a@gmail.com>
2018-05-15 11:37:59 +0100

    addedinitial auth servvice file and updated login component file

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+
+@Injectable()
+export class AuthService {
+
+  constructor(private router: Router) {}
+
+  login(authData: AuthData) {
+
+  }
+}
